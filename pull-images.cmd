2b0f2e447f2e4c16896289547a35f2acb3092e89
Uri Shaked <uri@salsa4fun.co.il>
2015-10-09 11:24:22 +0100

    add a script that pulls all the images for the demo

---
+++ pull-images.cmd
@@ -0,0 +1,9 @@
+docker pull hello-world
+docker pull python:2.7
+docker pull python:3.2
+docker pull wordpress
+docker pull orchardup/mysql
+docker pull node:4.0.0
+docker pull selenium/standalone-firefox
+docker pull selenium/standalone-chrome
+
